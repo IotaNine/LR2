5fdcb47a92b77ac02602e12b169fc37e11980d2e
IotaNine <70635434+IotaNine@users.noreply.github.com>
2021-01-21 16:41:22 +0300

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    for (int i = 0; i <= 256; i++) {
+    for (int i = 0; i <= 255; i++) {
         char n = i;
         std::cout << i << " " << n << std::endl;
     }
